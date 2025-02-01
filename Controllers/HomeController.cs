188a85bf01f65b344f5bf797726f661aa2aa9882
docdocxz <juliandiniro@gmail.com>
2025-02-01 11:48:58 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contador_para_Wizard_.Controllers {
+    public class HomeController : Controller {
+        [Route("/")]
+        [Route("/home")]
+        public IActionResult Home() {
+            return View();
+            }
+        }
+    }
